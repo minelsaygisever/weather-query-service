faf38147010a87f7df2b998b8a83fb2a2205489c
minelsaygisever <minelsaygisever@gmail.com>
2026-01-15 19:33:50 +0300

    feat: integrate weather api, add rest template config and setup global error handler

---
+++ src/main/java/com/minelsaygisever/weatherqueryservice/exception/ExternalServiceException.java
@@ -0,0 +1,7 @@
+package com.minelsaygisever.weatherqueryservice.exception;
+
+public class ExternalServiceException extends RuntimeException {
+    public ExternalServiceException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/minelsaygisever/weatherqueryservice/service/impl/WeatherServiceImpl.java
+++ src/main/java/com/minelsaygisever/weatherqueryservice/service/impl/WeatherServiceImpl.java
@@ -1,19 +1,50 @@
 package com.minelsaygisever.weatherqueryservice.service.impl;
 
+import com.minelsaygisever.weatherqueryservice.exception.ExternalServiceException;
 import com.minelsaygisever.weatherqueryservice.model.dto.WeatherResponse;
+import com.minelsaygisever.weatherqueryservice.model.dto.weatherapi.WeatherApiResponse;
 import com.minelsaygisever.weatherqueryservice.service.WeatherService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class WeatherServiceImpl implements WeatherService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${weather.api.url}")
+    private String apiUrl;
+
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     @Override
     public WeatherResponse getWeather(String location) {
-        log.info("Mock weather request received for location: {}", location);
+        log.info("Requesting weather data from WeatherAPI for location: {}", location);
 
-        // TODO: Returning mock data
-        return new WeatherResponse(location, 25.0);
+        String url = String.format("%s?key=%s&q=%s&days=1&aqi=no&alerts=no", apiUrl, apiKey, location);
+
+        try {
+            ResponseEntity<WeatherApiResponse> response = restTemplate.getForEntity(url, WeatherApiResponse.class);
+
+            if (response.getBody() != null && response.getBody().current() != null) {
+                Double tempC = response.getBody().current().tempC();
+
+                log.info("WeatherAPI returned temperature: {}C for {}", tempC, location);
+
+                return new WeatherResponse(location, tempC);
+            }
+        } catch (Exception e) {
+            log.error("Error fetching data from WeatherAPI: {}", e.getMessage());
+            throw new ExternalServiceException("Failed to fetch weather data: " + e.getMessage());
+        }
+
+        throw new ExternalServiceException("Weather data not found or API response invalid.");
     }
 }
