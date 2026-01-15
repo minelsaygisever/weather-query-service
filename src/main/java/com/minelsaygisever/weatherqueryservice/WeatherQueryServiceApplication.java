f48bfa322b78a9497b55327da397a12bb5108e95
minelsaygisever <minelsaygisever@gmail.com>
2026-01-15 14:14:22 +0300

    chore(init): initialize project with spring boot skeleton

---
+++ src/main/java/com/minelsaygisever/weatherqueryservice/WeatherQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minelsaygisever.weatherqueryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherQueryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherQueryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minelsaygisever/weatherqueryservice/WeatherQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minelsaygisever.weatherqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
