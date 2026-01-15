fb85a42c7aa7ba8731c5b4ea9303152cb2e2b3f4
minelsaygisever <minelsaygisever@gmail.com>
2026-01-15 20:44:07 +0300

    perf: refactor weather fetching to use async parallel execution

--- src/main/java/com/minelsaygisever/weatherqueryservice/service/impl/WeatherServiceImpl.java
+++ src/main/java/com/minelsaygisever/weatherqueryservice/service/impl/WeatherServiceImpl.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Slf4j
 @Service
@@ -16,19 +18,32 @@ import java.util.List;
 public class WeatherServiceImpl implements WeatherService {
 
     private final List<WeatherDataProvider> weatherProviders;
+    private final Executor taskExecutor;
 
     @Override
     public WeatherResponse getWeather(String location) {
-        log.info("Getting weather for {} from {} providers", location, weatherProviders.size());
+        log.info("Getting weather for {} asynchronously", location);
+
+        List<CompletableFuture<Double>> futures = weatherProviders.stream()
+                .map(provider -> CompletableFuture.supplyAsync(() -> {
+                    return provider.getCurrentTemperature(location);
+                }, taskExecutor))
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
         double totalTemp = 0.0;
         int successfulProviders = 0;
 
-        for (WeatherDataProvider provider : weatherProviders) {
-            Double temp = provider.getCurrentTemperature(location);
-            if (temp != null) {
-                totalTemp += temp;
-                successfulProviders++;
+        for (CompletableFuture<Double> future : futures) {
+            try {
+                Double temp = future.get();
+                if (temp != null) {
+                    totalTemp += temp;
+                    successfulProviders++;
+                }
+            } catch (Exception e) {
+                log.warn("One provider failed during async execution", e);
             }
         }
 
